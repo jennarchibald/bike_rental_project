4d4875da20a829c991f037965a51f9bcf4b5e46c
jennarchibald <codeclan@c192106.laplan.net.ed.ac.uk>
2019-03-22 14:49:01 +0000

    creating db tables

---
+++ controller.rb
@@ -0,0 +1,6 @@
+require('sinatra')
+require('sinatra/contrib/all')
+
+require_relative('./models/customer')
+require_relative('./models/lease')
+require_relative('./models/stock_item')
---
+++ db/bike_rental.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS leases;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS stock_items;
+
+CREATE TABLE customers (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  contact_number VARCHAR(255),
+  age INT2
+);
+
+CREATE TABLE stock_items (
+  id SERIAL4 PRIMARY KEY,
+  type VARCHAR(255),
+  rental_cost INT2
+);
+
+CREATE TABLE leases (
+  id SERIAL4 PRIMARY KEY,
+  start_date DATE,
+  end_date DATE,
+  customer_id INT4 REFERENCES customers(id) ON DELETE CASCADE,
+  stock_items_id INT4 REFERENCES stock_items(id) ON DELETE CASCADE
+);
