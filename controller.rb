cd66fec20ea3118b2e4327ca0d0ee74896941091
jennarchibald <codeclan@c192106.laplan.net.ed.ac.uk>
2019-03-22 14:50:10 +0000

    setting up controller with '/'

--- controller.rb
+++ controller.rb
@@ -4,3 +4,10 @@ require('sinatra/contrib/all')
 require_relative('./models/customer')
 require_relative('./models/lease')
 require_relative('./models/stock_item')
+
+also_reload('./models/*')
+
+
+get '/' do
+  erb(:home)
+end
