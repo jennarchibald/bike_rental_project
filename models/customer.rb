9110d4945ca97212ddec1f917763466b279dbe47
jennarchibald <codeclan@c192106.laplan.net.ed.ac.uk>
2019-03-22 15:04:15 +0000

    customer delete function

--- models/customer.rb
+++ models/customer.rb
@@ -23,6 +23,12 @@ class Customer
 
   # delete
 
+  def delete()
+    sql = 'DELETE FROM customers WHERE id = $1'
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
   def self.delete_all()
     sql = 'DELETE FROM customers'
     SqlRunner.run(sql)
