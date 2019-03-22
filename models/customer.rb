8be882cc09f2833f60a0bd0bd8f9b395c28668f1
jennarchibald <codeclan@c192106.laplan.net.ed.ac.uk>
2019-03-22 14:56:03 +0000

    customer class initialize

--- models/customer.rb
+++ models/customer.rb
@@ -1,1 +1,14 @@
 require_relative('../db/sql_runner.rb')
+
+class Customer
+  attr_accessor :name, :contact_number, :age
+  attr_reader :id
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+    @contact_number = options['contact_number']
+    @age = options['age'].to_i
+  end
+
+  
+end
