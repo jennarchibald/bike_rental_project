e4bef93533a71feeb210ed671867c90518873a6c
jennarchibald <codeclan@c192035.laplan.net.ed.ac.uk>
2019-03-23 13:41:11 +0000

    adding new and create routes to stock

--- controllers/stock_controller.rb
+++ controllers/stock_controller.rb
@@ -10,10 +10,19 @@ get '/stock' do
   erb(:"stock/index")
 end
 
-
+get '/stock/new' do
+  erb(:'stock/new')
+end
 
 
 get '/stock/:id' do
   @stock_item = StockItem.find_by_id(params[:id])
   erb(:'stock/show')
+end
+
+
+post '/stock' do
+  item = StockItem.new(params)
+  item.save()
+  redirect '/stock'
 end
