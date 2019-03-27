a328a6754974e4e43c8c4934ef00ba7202505e2f
jennarchibald <codeclan@CodeClans-MacBook-Pro.local>
2019-03-27 11:15:28 +0000

    adding sidebar to leased-items new

--- controllers/leased_items_controller.rb
+++ controllers/leased_items_controller.rb
@@ -5,6 +5,12 @@ require('sinatra/contrib/all')
 require_relative('../models/leased_item')
 also_reload('../models/*')
 
+get '/leased-items' do
+  @lease = Lease.find_by_id(params['lease_id'])
+  @items = StockItem.available_items()
+  erb(:'leased_items/new')
+end
+
 
 post '/leased-items/:lease_id/delete' do
   LeasedItem.delete_item_from_lease(params['item_id'], params['lease_id'])
