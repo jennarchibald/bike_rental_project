d8ed03455273a0c0588dd50ecd0056c34fdd31d1
jennarchibald <codeclan@CodeClans-MacBook-Pro.local>
2019-03-27 10:38:39 +0000

    refacting leased-items controller

--- controllers/leased_items_controller.rb
+++ controllers/leased_items_controller.rb
@@ -5,16 +5,11 @@ require('sinatra/contrib/all')
 require_relative('../models/leased_item')
 also_reload('../models/*')
 
-post '/leased-items' do
-  @items = StockItem.available_items()
-  @lease = Lease.find_by_id(params['lease_id'])
-  erb(:'leased_items/new')
-end
 
-post '/leased-items/:id/delete' do
+post '/leased-items/:lease_id/delete' do
   LeasedItem.delete_item_from_lease(params['item_id'], params['lease_id'])
   # binding.pry()
-  @lease = Lease.find_by_id(params[:id])
+  @lease = Lease.find_by_id(params[:lease_id])
   @items = StockItem.available_items()
   erb(:'leased_items/new')
 end
